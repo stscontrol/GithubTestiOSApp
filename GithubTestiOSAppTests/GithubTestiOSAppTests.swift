6b0dfcea3f6453196499a69d4eb6708878343a5b
stscontrol <stscontrolpak@gmail.com>
2024-11-21 13:12:20 +0500

    Initial Commit

---
+++ GithubTestiOSAppTests/GithubTestiOSAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestiOSAppTests.swift
+//  GithubTestiOSAppTests
+//
+//  Created by D2Vision on 21/11/2024.
+//
+
+import Testing
+@testable import GithubTestiOSApp
+
+struct GithubTestiOSAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
